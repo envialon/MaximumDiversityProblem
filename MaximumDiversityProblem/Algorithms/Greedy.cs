7f3385420425d1c58b7c459e8c8c405257779d5b
Enrique Viña Alonso <alu0101337760@ull.edu.es>
2022-05-05 22:54:27 +0100

    Last changes, makeRCL not finished

--- MaximumDiversityProblem/Algorithms/Greedy.cs
+++ MaximumDiversityProblem/Algorithms/Greedy.cs
@@ -3,10 +3,15 @@
     public class Greedy
     {             
 
-        static private List<List<double>> MakeRCL(List<double> centroid, List<List<double>> availableNodes, int rclSize)
+        static private List<List<double>> MakeRCL(List<List<double>> distanceMatrix, List<List<double>> availableNodes, List<double> centroid, int rclSize)
         {
             List<List<double>> rcl = new List<List<double>>();
 
+            for (int i = 0; i < availableNodes.Count; i++)
+            {
+                double distance = 0;
+               
+            }
 
             return rcl;
         }
@@ -16,12 +21,17 @@
             Random rand = new Random();
             Solution solution = new Solution(problem);
             List<List<double>> availableVectors = new List<List<double>>();
+            List<List<double>> distanceMatrix = problem.distanceMatrix;
             int numberOfVectors = problem.numberOfVectors;
 
             for (int i = 0; i < numberOfVectors; i++)
             {
                 List<double> centroid = Utils.GetCentroid(availableVectors, solution.solution);
-                List<List<double>> rcl = MakeRCL(centroid, availableVectors, rclSize);
+                List<List<double>> rcl = MakeRCL(distanceMatrix, availableVectors, centroid, rclSize);
+                if (rcl.Count > 0)
+                {
+                    break;
+                }
                 int index = rand.Next(0, rcl.Count);
                 solution.solution.Add(index);
                 availableVectors.RemoveAt(index);
