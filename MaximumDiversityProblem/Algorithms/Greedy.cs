78bda4cecd95deafcb4b07aeaa9731aff13f5628
Enrique Viña Alonso <alu0101337760@ull.edu.es>
2022-05-05 21:15:35 +0100

    Changes before deciding to build a distanceMatrix

--- MaximumDiversityProblem/Algorithms/Greedy.cs
+++ MaximumDiversityProblem/Algorithms/Greedy.cs
@@ -1,6 +1,6 @@
 ﻿namespace MaximumDiversityProblem
 {
-    public class Greedy 
+    public class Greedy
     {
 
         static private List<double> GetCentroid(List<List<double>> availableVectors, HashSet<int> solutionSet)
@@ -26,13 +26,28 @@
             return result;
         }
 
+        static private double GetDistance(List<double> centroid, List<double> target)
+        {
+            double result = 0;
 
+            for (int i = 0; i < centroid.Count; i++)
+            {
+                result += (centroid[i] - target[i]) * (centroid[i] - target[i]);
+            }
+
+            return Math.Sqrt(result);
+        }
 
         static private List<List<double>> MakeRCL(List<double> centroid, List<List<double>> availableNodes, int rclSize)
         {
             List<List<double>> rcl = new List<List<double>>();
+            List<(int, double)> indexDistanceList = new List<(int, double)>();
 
-
+            for (int i = 0; i < availableNodes.Count; i++)
+            {
+                double distance = GetDistance(centroid, availableNodes[i]);
+                
+            }
 
             return rcl;
         }
@@ -40,17 +55,17 @@
         static public Solution Solve(Problem problem, int rclSize)
         {
             Random rand = new Random();
-            Solution solution = new Solution(problem); 
+            Solution solution = new Solution(problem);
             List<List<double>> availableVectors = new List<List<double>>();
-            int numberOfVectors = problem.numberOfVectors;     
+            int numberOfVectors = problem.numberOfVectors;
 
-            for( int i = 0; i < numberOfVectors; i++)
+            for (int i = 0; i < numberOfVectors; i++)
             {
                 List<double> centroid = GetCentroid(availableVectors, solution.solution);
                 List<List<double>> rcl = MakeRCL(centroid, availableVectors, rclSize);
                 int index = rand.Next(0, rcl.Count);
                 solution.solution.Add(index);
-                availableVectors.RemoveAt(index);     
+                availableVectors.RemoveAt(index);
             }
 
             return solution;
