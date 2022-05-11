b8207fadbeadcaf1c3ba969f26dde1256d4e015d
Enrique Viña Alonso <alu0101337760@ull.edu.es>
2022-05-11 09:38:20 +0100

    LastChanges

--- NLP_IAA/Program.cs
+++ NLP_IAA/Program.cs
@@ -4,13 +4,13 @@
     {
         static void Main(string[] args)
         {
-            ProbabiltyEstimation.GoodModel("COV_train.xlsx", "vocabulario.txt");
+            //ProbabiltyEstimation.GoodModel("COV_train.xlsx", "vocabulario.txt");
 
 
 
             Console.WriteLine("Introduce the test filename:");
             //string testFile = Console.ReadLine();
-            string testFile = "COV_test_sample.xlsx";
+            string testFile = "COV_train.xlsx";
             Classifier.Classify(testFile);
             Console.WriteLine(Classifier.Validate());
         }
